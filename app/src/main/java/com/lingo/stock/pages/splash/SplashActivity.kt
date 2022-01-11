a7f3abe2038116312bb53b3f9f8fd0ef5d0e4a03
niulinguo <niulinguo@163.com>
2022-01-11 17:01:06 +0800

    Initial commit

---
+++ app/src/main/java/com/lingo/stock/pages/splash/SplashActivity.kt
@@ -0,0 +1,7 @@
+package com.lingo.stock.pages.splash
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity()
